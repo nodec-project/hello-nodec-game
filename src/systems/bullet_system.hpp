e327ae0daa14f633246489ebfd386a83ad58458e
IOE <fivetwothreesix@gmail.com>
2023-08-12 21:26:35 +0900

    update engine
    fix compatibility

--- src/systems/bullet_system.hpp
+++ src/systems/bullet_system.hpp
@@ -7,49 +7,50 @@
 namespace systems {
 
 class BulletSystem {
+    struct DestroyLater {};
+
 public:
     BulletSystem(nodec_world::World &world, nodec_physics::systems::PhysicsSystem &physics_system) {
         using namespace nodec_physics;
         using namespace nodec;
+        using namespace nodec_scene;
+        using namespace ::components;
 
         world.stepped().connect([&](nodec_world::World &world) { on_stepped(world); });
 
-        // physics_system.collision_stay().connect([&](const CollisionInfo &info) {
-        //     if (world.scene().registry().try_get_component<Bullet>(info.entity0)) {
-        //         world.scene().registry().destroy_entity(info.entity0);
-        //     }
-        //     if (world.scene().registry().try_get_component<Bullet>(info.entity1)) {
-        //         world.scene().registry().destroy_entity(info.entity1);
-        //     }
-        // });
+        physics_system.collision_stay().connect([&](const CollisionInfo &info) {
+            auto &scene_registry = world.scene().registry();
+
+            auto process = [&](SceneEntity current) {
+                if (!scene_registry.try_get_component<Bullet>(current)) return;
+                scene_registry.emplace_component<DestroyLater>(current);
+            };
+            process(info.entity0);
+            process(info.entity1);
+        });
     }
 
+private:
     void on_stepped(nodec_world::World &world) {
         using namespace nodec_scene;
         using namespace nodec_physics::components;
         using namespace ::components;
 
-        world.scene().registry().view<Bullet>().each([&](auto entt, Bullet &bullet) {
+        auto &scene_registry = world.scene().registry();
+
+        scene_registry.view<Bullet>().each([&](auto entt, Bullet &bullet) {
             bullet.lifetime -= world.clock().delta_time();
 
             if (bullet.lifetime < 0.f) {
-                to_deletes_.emplace_back(entt);
+                scene_registry.emplace_component<DestroyLater>(entt);
             }
         });
 
-        world.scene().registry().destroy_entities(to_deletes_.begin(), to_deletes_.end());
-        to_deletes_.clear();
-
-        world.scene().registry().view<Bullet, CollisionStay>().each([&](auto entt, Bullet, CollisionStay) {
-            to_deletes_.emplace_back(entt);
-        });
-
-        world.scene().registry().destroy_entities(to_deletes_.begin(), to_deletes_.end());
-        to_deletes_.clear();
+        {
+            auto view = scene_registry.view<DestroyLater>();
+            scene_registry.destroy_entities(view.begin(), view.end());
+        }
     }
-
-private:
-    std::vector<nodec_scene::SceneEntity> to_deletes_;
 };
 } // namespace systems
 
